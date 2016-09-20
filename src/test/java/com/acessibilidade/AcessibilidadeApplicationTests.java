e1f86b218ddce4c53dc75c9cad1943e8ba900b32
Raphael <raphaelc23@gmail.com>
2016-09-20 14:19:56 -0300

    Primeiro Commit

---
+++ src/test/java/com/acessibilidade/AcessibilidadeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.acessibilidade;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AcessibilidadeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
