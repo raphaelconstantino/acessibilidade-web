68f7358d7b7e09a66ed2cbf0e1bdbe48d30184ca
raphaelconstantino <raphaelc23@gmail.com>
2016-09-20 18:27:47 -0300

    PC Pessoal

---
+++ src/main/java/com/acessibilidade/AcessibilidadeApplication.java
@@ -0,0 +1,12 @@
+package com.acessibilidade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcessibilidadeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcessibilidadeApplication.class, args);
+	}
+}
