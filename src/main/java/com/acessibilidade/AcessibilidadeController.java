acf9fefecbc467db70fcdfb81466b53d01d27b74
raphaelconstantino <raphaelc23@gmail.com>
2016-09-20 21:06:22 -0300

    Adicionando thymeleaf

---
+++ src/main/java/com/acessibilidade/AcessibilidadeController.java
@@ -0,0 +1,46 @@
+package com.acessibilidade;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class AcessibilidadeController {
+
+    @RequestMapping("/")
+    public String index(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "index";
+    }
+
+    @RequestMapping("/tabelas")
+    public String tabelas(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "tabelas";
+    }
+	
+    @RequestMapping("/formularios")
+    public String formularios(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "formularios";
+    }	
+	
+	@RequestMapping("/imagens")
+    public String imagens(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "imagens";
+    }	
+	
+	@RequestMapping("/cores")
+    public String cores(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "cores";
+    }		
+	
+	@RequestMapping("/medidas")
+    public String medidas(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "medidas";
+    }	
+}
