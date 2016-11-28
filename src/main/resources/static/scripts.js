019e0925ce810b57b1e23cd303a8e07d4a31b24e
Raphael <raphaelc23@gmail.com>
2016-11-28 17:34:48 -0200

    Fix dp drop

--- src/main/resources/static/scripts.js
+++ src/main/resources/static/scripts.js
@@ -1,5 +1,17 @@
 $(document).ready({
 	
-	console.log("a");
+	var path = window.location.pathname;
+	var $menuTop = $("#menuTop");
+	
+	if (path == "/" || path == "/roles" || path == "/tabelas" || path == "/imagens" || path == "/formularios" || path == "/msgFormularios" || path == "/progressBar" || path == "/tabs" || path == "/rolesMenus" || path == "/modal" ||)
+	{
+		$menuTop.find(".inicio").addClass("active");
+	} else if (path == "/sobre") {
+		$menuTop.find(".sobre").addClass("active");		
+	} else if (path == "/infos") {
+		$menuTop.find(".infos").addClass("active");		
+	} else if (path == "/contato") {
+		$menuTop.find(".contato").addClass("active");		
+	}
 	
 });
