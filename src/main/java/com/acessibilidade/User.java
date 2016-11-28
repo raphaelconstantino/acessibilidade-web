88b4589fc975c87588dbe9e877a225ac6ce7d928
raphaelconstantino <raphaelc23@gmail.com>
2016-11-28 20:19:55 -0200

    Ajustando area adm

--- src/main/java/com/acessibilidade/User.java
+++ src/main/java/com/acessibilidade/User.java
@@ -14,7 +14,7 @@ public class User {
 
     private String username;
     private String password;
-    private String eemail;
+    private String email;
  
     public String getUsername() {
         return username;
