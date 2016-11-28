badfc99a43cc56e1c0d6223aafde4061f89533f7
raphaelconstantino <raphaelc23@gmail.com>
2016-11-28 21:19:24 -0200

    Ajustando area adm

--- src/main/java/com/acessibilidade/UserRepository.java
+++ src/main/java/com/acessibilidade/UserRepository.java
@@ -3,9 +3,7 @@ package com.acessibilidade;
 import java.util.List;
 
 
-import org.springframework.data.jpa.repository.Query; 
 import org.springframework.data.repository.PagingAndSortingRepository; 
-import org.springframework.data.repository.query.Param; 
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.acessibilidade.User;
@@ -13,5 +11,5 @@ import com.acessibilidade.User;
 @RepositoryRestResource(collectionResourceRel = "user", path = "users")
 public interface UserRepository extends PagingAndSortingRepository<User, Long> {
 
-	List<User> findByUsername(@Param("username") String username);
+	
 }
