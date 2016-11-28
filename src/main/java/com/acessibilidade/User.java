70dee2b841e8d982dc59b55e53e9d487a9d9d971
raphaelconstantino <raphaelc23@gmail.com>
2016-11-28 20:34:01 -0200

    Ajustando area adm

--- src/main/java/com/acessibilidade/User.java
+++ src/main/java/com/acessibilidade/User.java
@@ -23,10 +23,6 @@ public class User {
     public void setId(Long id) {
           this.id = id;
     }
-
-    public String getNome() {
-          return nome;
-    }
  
     public String getUsername() {
         return username;
