655b89f974532f532d71e38a3d42957ac50719f8
raphaelconstantino <raphaelc23@gmail.com>
2016-11-27 22:06:49 -0200

    Add add info

--- src/main/java/com/acessibilidade/WebSecurityConfig.java
+++ src/main/java/com/acessibilidade/WebSecurityConfig.java
@@ -6,7 +6,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter
+import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
 
 @Configuration
 @EnableWebSecurity
