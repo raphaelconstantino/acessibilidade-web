8d06c36a51af57414ef2cfcccb2a0a2f8d8ab170
raphaelconstantino <raphaelc23@gmail.com>
2016-11-28 20:32:13 -0200

    Ajustando area adm

--- src/main/java/com/acessibilidade/User.java
+++ src/main/java/com/acessibilidade/User.java
@@ -9,12 +9,24 @@ import javax.persistence.Id;
 public class User {
  
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private long id;
+    @GeneratedValue(strategy=GenerationType.AUTO)    
+    private Long id;
 
     private String username;
     private String password;
     private String email;
+
+    public Long getId() {
+          return id;
+    }
+
+    public void setId(Long id) {
+          this.id = id;
+    }
+
+    public String getNome() {
+          return nome;
+    }
  
     public String getUsername() {
         return username;
