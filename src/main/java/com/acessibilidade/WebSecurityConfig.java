dd5adde4ba1e092e8b90f24c2fc9fe4aa1512838
raphaelconstantino <raphaelc23@gmail.com>
2016-11-28 22:09:38 -0200

    Ajustando area adm

--- src/main/java/com/acessibilidade/WebSecurityConfig.java
+++ src/main/java/com/acessibilidade/WebSecurityConfig.java
@@ -38,7 +38,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
         
-        List<Usuario> users = jdbcTemplate.queryForList("SELECT * FROM usuarios");
+        List<java.util.Map<java.lang.String, Usuario> users = jdbcTemplate.queryForList("SELECT * FROM usuarios");
 
         auth
             .inMemoryAuthentication()
